da2b9b8058a9d7113348b7c3f2bc4aa3b74f0cc7
Dan Morrison <danielroy.morrison@gmail.com>
2017-02-03 13:12:22 +1300

    set variable left offset for sick manu dude, dependant on screen width

--- scripts/manu.js
+++ scripts/manu.js
@@ -1,16 +1,16 @@
 var manu = function(){
   $("#massive-manu").on("click", function(){
     $("#sick-manu").css("top", "-200px")
-    for (i = 0; i < 500; i++ ){
+    for (i = 0; i < 700; i++ ){
       downwardManu(i)
     }
   })
-}
 
-function downwardManu(i){
-  setTimeout(function(){
-    $("#sick-manu").css("top", i+"px")
-  },i * 1.5)
+  function downwardManu(i){
+    setTimeout(function(){
+      $("#sick-manu").css("top", i+"px")
+    },i * 1.5)
+  }
 }
 
 $(document).ready(manu);
