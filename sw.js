7b756d9bb5bd5fec82a46b0348097e7258235ddc
Dan Morrison <danielroy.morrison@gmail.com>
2017-02-07 20:08:05 +1300

    switch place of service worker javascript for proper scoping, edit scope in index, revise pathways for cached file

--- sw.js
+++ sw.js
@@ -2,10 +2,11 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('sickmanu').then(function(cache) {
       return cache.addAll([
-      '/index.html',
+      '/',
       '/styles/style.css',
       '/scripts/manu.js',
       '/scripts/jquery-3.1.1.min.js',
+      '/images/chea_hooey.png',
       '/HOO_.mp4'
       ]);
     })
@@ -13,6 +14,7 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('fetch', function(event) {
+  console.log("yes")
   console.log(event.request.url);
   event.respondWith(
     caches.match(event.request).then(function(response) {
